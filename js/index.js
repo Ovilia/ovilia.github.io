ccbcf810df085f70808c5ae213f664037025e630
Ovilia <zwl.sjtu@gmail.com>
2013-02-04 10:43:33 +0800

    Add google analytics

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-38205696-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 var isMSIE = /*@cc_on!@*/0;
 if (isMSIE) {
     location.href = 'no_ie.html';
