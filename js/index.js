f4ea1752f1c2ef83a0c9fc8ef1b2e1235f484b44
Wenli Zhang <zwl.sjtu@gmail.com>
2013-10-16 20:52:02 +0800

    New version of site under construction! :smirk:

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var _gaq = _gaq || [];
+/*var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-38205696-1']);
 _gaq.push(['_trackPageview']);
 
@@ -6,21 +6,22 @@ _gaq.push(['_trackPageview']);
 var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+})();*/
 
 var isMSIE = /*@cc_on!@*/0;
 if (isMSIE) {
     location.href = 'no_ie.html';
 }
 
+var STATE = {
+    HOME: 0,
+    LANGUAGE: 1,
+    TOPIC: 2,
+    CONTENT: 3
+};
+state = STATE.HOME;
+
 $(document).ready(function() {
-    // font related
-    $('#areMy').fitText(0.92).shuffleLetters({
-        fps: 20,
-        steps: 16
-    });
-    $('#inTao').fitText(0.5).hide().fadeIn(2000);
-    
     // window scroll
     $(window).scroll(function() {
         if (window.innerWidth > 600) {
@@ -46,13 +47,22 @@ $(document).ready(function() {
                 $('#navContent').slideUp();
             }
         }
-    });
-    
-    $('.navCnt').click(function() {
-        var id = '#' + $(this).attr('id').slice(0, -3) + 'Page';
-        $('body').animate({
-            scrollTop: $(id).offset().top - 150
-        });
+    }).mousemove(function(e) {
+        if (state != STATE.CONTENT) {
+            // shadow moves with mouse
+            var x = (0.5 - e.clientX / $(window).width()) * 2;
+            var y = (0.5 - e.clientY / $(window).height()) * 2;
+            var r = Math.sqrt((x * x + y * y) / 2);
+            var op = 1 - Math.sqrt(r);
+            $('#titleCircle').css({
+                'box-shadow': 300 * x + 'px ' + 300 * y + 'px '
+                    + 300 * r + 'px #333',
+                'opacity': op
+            });
+            $('#titleLeft, #titleRight').css({
+                'opacity': op
+            });
+        }
     });
     
     // randocy image
@@ -67,4 +77,22 @@ $(document).ready(function() {
     }, function() {
         $('#jWebAudioImg').hide().attr('src', 'image/jWebAudio_gray.png').show();
     });
+    
+    taoRotate();
 });
+
+function taoRotate() {
+    var time = 0;
+    var id = setInterval(function() {
+        if (time <= 60) {
+            $('#titleDiv').css('margin-top', $(window).height() / 2 - 300
+                               + (time - 60))
+                .css('opacity', time / 60);
+            $('#titleTao').rotate(360 * Math.cos(Math.PI / 60 * time) + 'deg');
+            time += 1;
+        } else {
+            $('#titleTao').rotate(0);
+            clearInterval(id);
+        }
+    }, 50);    
+}
