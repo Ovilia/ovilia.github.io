b17568fd2d18a3f981bf99278ea53c3e7c9413ab
Ovilia <zwl.sjtu@gmail.com>
2014-08-29 22:00:30 +0800

    font hosted on blog/font now

--- js/index.js
+++ js/index.js
@@ -78,16 +78,3 @@ function taoRotate() {
         }
     }, 50);    
 }
-
-WebFontConfig = {
-    google: { families: [ 'Lato:400,900:latin' ] }
-};
-(function() {
-    var wf = document.createElement('script');
-    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-})();
